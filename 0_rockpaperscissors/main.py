75a9139f4ec0c9751d75cd2b1724a2031e2d4d32
Yadkee <yadkee@gmail.com>
2019-01-20 23:01:13 +0100

    beautified the code

--- 0_rockpaperscissors/main.py
+++ 0_rockpaperscissors/main.py
@@ -1,4 +1,5 @@
 #! python3
+import collections
 import random
 
 import numpy as np
@@ -7,95 +8,138 @@ import numpy as np
 #             -> http://iamtrask.github.io/2015/07/27/python-network-part2/
 # And -> http://iamtrask.github.io/2015/11/15/anyone-can-code-lstm/
 #     -> http://iamtrask.github.io/2015/07/28/dropout/
-OPTIONS = set("123")
-NAMES = ("rock", "paper", "scissors")
-EXIT = {"0", "q", "exit"}
 
 
-def nonlin(x, deriv=False):
-    if deriv:
-        return x * (1 - x)
+def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def create(layers):
-    return [2 * np.random.random((layers[a], layer)) - 1
-            for a, layer in enumerate(layers[1:])]
+def sigmoid_derivative(x):
+    return x * (1 - x)
 
 
-def train(x, y, weights, epochs):
-    for i in range(epochs):
+class Model:
+    def __init__(self, layers):
+        self.weight = [2 * np.random.random((layers[a], layer)) - 1
+                       for a, layer in enumerate(layers[1:])]
+
+    def forward(self, x):
         layer = [x]
-        for weight in weights:
-            layer.append(nonlin(np.dot(layer[-1], weight)))
+        for weight in self.weight:
+            layer.append(sigmoid(np.dot(layer[-1], weight)))
+        return layer
 
-        error = [y - layer[-1]]
-        delta = [error[0] * nonlin(layer[-1], deriv=True)]
-        if not i or i == epochs - 1:
-            print(f"Error: {np.mean(np.abs(error[0]))}")
+    def train(self, x, y, epochs):
+        weight = self.weight
+        lw = len(weight)
+        forward = self.forward
+        for i in range(epochs):
+            layer = forward(x)
 
-        for i in range(1, len(weights)):
-            error.insert(0, delta[0].dot(weights[-i].T))
-            delta.insert(0, error[0] * nonlin(layer[-i - 1], deriv=True))
-        weights = [weight + layer[a].T.dot(delta[a])
-                   for a, weight in enumerate(weights)]
-    return weights
+            error = [y - layer[-1]]
+            delta = [error[0] * sigmoid_derivative(layer[-1])]
+            if not i or i == epochs - 1:
+                print(f"Error: {np.mean(np.abs(error[0]))}")
 
+            for i in range(1, lw):
+                error.insert(0, delta[0].dot(weight[-i].T))
+                delta.insert(0, error[0] * sigmoid_derivative(layer[-i - 1]))
 
-def test(x, weights):
-    layer = [x]
-    for weight in weights:
-        layer.append(nonlin(np.dot(layer[-1], weight)))
-    return layer[-1]
+            for i in range(lw):
+                weight[i] += layer[i].T.dot(delta[i])
+
+    def save(self, name):
+        for a, w in enumerate(self.weight):
+            np.save(f"{name}_{a}.npy", w)
+
+    def load(self, name):
+        self.weight = [np.load(f"{name}_{a}.npy")
+                       for a in range(len(self.weight))]
 
 
 def main():
-    print("0.Exit\n1. Rock\n2. Paper\n3. Scissors\n\n")
     layers = [10, 90, 30, 3]
-    weights = create(layers)
-    counter = [0, 0, 0]
-    last = []
-    x = np.array([last[-layers[0]:]])
+    model = Model(layers)
+
+    t = 0.5  # Tie value
+    correctChoice = [np.array([[t, 1, 0]]),
+                     np.array([[0, t, 1]]),
+                     np.array([[1, 0, t]])]
+
+    counter = [0, 0, 0, 0]
+    last = collections.deque((i % 3 for i in range(10)), maxlen=layers[0])
+    x = np.array([last])
+
+    helpText = """
+    0.Exit
+    1. Rock
+    2. Paper
+    3. Scissors
+
+    h -> help
+    q -> quit
+    s -> save
+    l -> load
+    """
+    numbers = {"1": 0, "2": 1, "3": 2}
+    names = ("rock", "paper", "scissors")
+    outcomes = ("Win", "Tie", "Lose")
+    EXIT = {"0", "q", "quit", "exit"}
+
+    print(helpText)
+
     while True:
-        option = input("> ")
+        ignore = True
+        try:
+            option = input("> ")
+        except KeyboardInterrupt:
+            option = "exit"
+
         if option in EXIT:
             print("Goodbye!")
             break
-        if option == "save":
-            for a, w in enumerate(weights):
-                np.save(f"save_{a}.npy", w)
-            continue
-        elif option == "load":
-            weights = [np.load(f"save_{a}.npy") for a in range(len(weights))]
-            continue
-        if option not in OPTIONS:
-            print("That was not between 1 and 3 included")
-            continue
-        option = int(option) - 1
-        last.append(option)
+        elif option == "h" or option == "help":
+            print(helpText)
+        elif option == "s" or option == "save":
+            model.save("save")
+        elif option == "l" or option == "load":
+            model.load("save")
+        elif option not in numbers:
+            print("That was not 1, 2 or 3")
+        else:
+            ignore = False
 
-        if len(last) > layers[0] + 1:
-            machine = max((i, a) for a, i in enumerate(test(x, weights)[0]))[1]
+        if ignore:
+            continue
+
+        number = numbers[option]
+        predictions = model.forward(x)[-1][0]
+        machine = max(enumerate(predictions), key=lambda x: x[1])[0]
+
+        if (machine + 1) % 3 == number % 3:
+            o = 0
+            print("[%8s] vs  %8s " % (names[number], names[machine]))
+        elif machine == number:
+            o = 1
+            print(" %8s  vs  %8s " % (names[number], names[machine]))
         else:
-            machine = random.randint(0, 2)
-        print("[%8s] vs [%8s]" % (NAMES[option], NAMES[machine]))
-        if (machine + 1) % 3 == option % 3:
-            s = "Win"
-            counter[0] += 1
-        elif machine == option:
-            s = "Tie"
-            counter[1] += 1
-        else:
-            s = "Lose"
-            counter[2] += 1
-        print("%4s   %d | %d | %d (w|t|l) %.2f | %.2f | %.2f" %
-              (s, *counter, counter[0] / sum(counter),
-               counter[1] / sum(counter), counter[2] / sum(counter)))
-        if len(last) > layers[0] + 1:
-            y = np.array([[0 if ((i + 1) % 3 == option % 3) else
-                           (0.5 if i == option else 1) for i in range(3)]])
-            weights = train(x, y, weights, 1000)
-        x = np.array([last[-layers[0]:]])
+            o = 2
+            print(" %8s  vs [%8s]" % (names[number], names[machine]))
+        counter[o] += 1
+        counter[3] += 1
+        print("%4s   %03d | %03d | %03d (w|t|l) %.2f | %.2f | %.2f" %
+              (outcomes[o], *counter[:3],
+               counter[0] * 100 // counter[3],
+               counter[1] * 100 // counter[3],
+               counter[2] * 100 // counter[3]))
+
+        if counter[3] >= layers[0]:
+            y = correctChoice[number]
+            model.train(x, y, 1000)
+
+        last.append(number)
+        last.append(machine)
+        x = np.array([last])
 
 
 if __name__ == "__main__":
